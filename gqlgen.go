932056143ae3c8c094340a70be39c2d97579bc65
anoriqq <shota.yoshikawa@anoriqq.com>
2025-07-07 23:12:55 +0900

    feat: use errors.Errorf instead of fmt.Errorf

--- gqlgen.go
+++ gqlgen.go
@@ -1,6 +1,7 @@
 package main
 
 // Arguments to format are:
+//
 //	[1]: type name
 const gqlgenMethods = `
 // MarshalGQL implements the graphql.Marshaler interface for %[1]s
@@ -12,7 +13,7 @@ func (i %[1]s) MarshalGQL(w io.Writer) {
 func (i *%[1]s) UnmarshalGQL(value interface{}) error {
 	str, ok := value.(string)
 	if !ok {
-		return fmt.Errorf("%[1]s should be a string, got %%T", value)
+		return errors.Errorf("%[1]s should be a string, got %%T", value)
 	}
 
 	var err error
